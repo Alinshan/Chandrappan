e6066823fe7624e67f4dd9e157626d930dd64f2e
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-08-07 19:35:27 +0530

    Update cslogo.js

--- plugins/cslogo.js
+++ plugins/cslogo.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/gplaybutton?apikey=bY17wlPg4XQvRQkJRqXjPPipPd2&text=$%20=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Sophia*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*MADE BY CHANDRAPPAN*' })
 
     }));
 }
@@ -29,7 +29,7 @@ Asena.addCommand({pattern: 'cs ?(.*)', fromMe: false, dontAddCommandList: true},
 
     var webimage = await axios.get(`https://api.zeks.xyz/api/cslogo?apikey=bY17wlPg4XQvRQkJRqXjPPipPd2&text=${match[1]}`, { responseType: 'arraybuffer' })
 
-  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: '*MADE BY POOCHA_SER*'  })
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: '*MADE BY CHANDRAPPAN*'  })
 
 }));
     
