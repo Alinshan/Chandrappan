19d57a7c1c73cf5678e7bb10fdbd75f04db56003
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-08-07 19:16:23 +0530

    Update alinshan.js

--- plugins/alinshan.js
+++ plugins/alinshan.js
@@ -32,7 +32,7 @@ if (Config.WORKTYPE == 'private') {
 
 		const json = JSON.parse(response.body);
 
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*POOCHA SER* 😘' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*CHANDRAPPAN* 😘' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 
 	} catch {
 
@@ -58,7 +58,7 @@ else if (Config.WORKTYPE == 'public') {
 
 		const json = JSON.parse(response.body);
 
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*POOCHA SER* 😘' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*CHANDRAPPAN* 😘' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 
 	} catch {
 
