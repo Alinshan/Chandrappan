cc3f35c3a5239d0c125371d59e642e5b9442fbc0
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-08-09 18:13:11 +0530

    Update liya.js

--- plugins/liya.js
+++ plugins/liya.js
@@ -18,7 +18,7 @@ const lol = "need some word\n💘enthelum ezhuthu nenba"
 
         var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/ttlogo?text1=${encodeURIComponent(match[1])}&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Amalser🧸*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*MADE BY BOT CHANDRAPPAN🧸*' })
 
     }));
 }
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/ttlogo?text1=${encodeURIComponent(match[1])}&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Amalser🧸*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*MADE BY BOT CHANDRAPPAN🧸*' })
 
     }));
     
