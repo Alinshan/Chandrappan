4329169729273eeb0fd398510c13e7c64e509987
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-08-09 18:02:37 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -191,7 +191,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by WhatsAsenaPublic'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'MADE BY BOT CHANDRAPPAN'})
 
     }));
 }
