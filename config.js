e3c25de05c13024502b8a5e69929f52b3f3d0095
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-08-09 17:48:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2021 Amal-ser.
-re edited by afnanplk  
+re edited by ALINSHAN  
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
